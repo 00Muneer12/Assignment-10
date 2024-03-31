49f7ee20fbc33549555b5ac22dbf2268a104c31c
“[firstname <[gmugsk@gmail.com]>
2024-03-31 14:23:29 +0500

    Assignment 10

---
+++ main.js
@@ -0,0 +1,16 @@
+// Assignment 10
+/*Adding Comments: Choose two of the programs
+ you’ve written, and add at least one comment
+  to each. If you don’t have anything specific
+  to write because your programs are too simple
+   at this point, just add your name and the current
+   date at the top of each program file. Then write
+   one sentence describing what the program does.*/
+// This program adds two numbers
+//addition
+var numberOne = 10;
+var numberTwo = 20;
+//Result (Answer)
+var sumNumber = numberOne + numberTwo;
+//Print result
+console.log(sumNumber);
---
+++ main.ts
@@ -0,0 +1,21 @@
+// Assignment 10
+/*Adding Comments: Choose two of the programs
+ you’ve written, and add at least one comment
+  to each. If you don’t have anything specific 
+  to write because your programs are too simple
+   at this point, just add your name and the current 
+   date at the top of each program file. Then write 
+   one sentence describing what the program does.*/
+
+
+// This program adds two numbers
+//Ctrl+question
+//addition
+let numberOne: number = 10;
+let numberTwo: number = 20;
+//Add two numbers
+let result: number = numberOne + numberTwo;
+//Result (Answer)
+console.log(result);
+
+
